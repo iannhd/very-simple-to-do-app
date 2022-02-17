686fa04da37a0d649438fdf511b6344bc5444aae
iannhd <in.hadyan@gmail.com>
2022-02-18 05:54:03 +0700

    there is still problem when reloading, the content of object is still not reappear

--- index.js
+++ index.js
@@ -1,32 +1,84 @@
-const todo = document.getElementById('todo')
+let todos = {} // perantara Local Storage
+const STORAGE_TODO = 'STORAGE_TODO'
+const todoBox = document.getElementById('todo')
+
+
+    // ==============================================
+    // =====    LOCAL STORAGE =======================
+    // ==============================================
+    
+    // Check if localStorage API is available
+    if (typeof (Storage) !== "undefined")
+        console.log('Local Storage Available');
+    else
+        console.log('Oops, your data will gone after page reload');
+        
+
+    // read localStorage on first load
+    if (todoFromLocal = localStorage.getItem(STORAGE_TODO)) {
+        todos = JSON.parse(todoFromLocal)
+
+    // loop isi object todos bikin listnya
+        for (let key in todos) {
+        createList()
+        }
+    }
+    function syncLocalStorage(activity, item, status =false) {
+        switch (activity) {
+            case 'ADD':
+            case 'UPDATE':
+                 todos[item] = status
+                break
+            case 'DELETE':
+                delete todos[item]
+                break
+            default:
+                break
+        }
+        
+        console.log(todos);
+
+        localStorage.setItem(STORAGE_TODO, JSON.stringify(todos))
+        return
+    }
+    
+    // ==============================================
+    // =====    TODO FUNCTIONS =======================
+    // ==============================================
+
 
 function add() {
     //ambil nilai dari textnya
     let newText = document.getElementById('new_text')
 
     //tambahkan list baru ke dalam ul
-    let newTodo = `<li class="the_task"> 
-                   <span class='newLi' onclick="toggle(this)">  ${newText.value} </span> 
-                   <span onclick="removeItem(this)">  [x] </span>
-                   </li>`
-    
+    createList(newText.value)
 
-    // let li = document.createElement(`li`)
-    // let txt = document.createTextNode(newText.value)
+    syncLocalStorage('ADD', newText.value)
 
-    // li.appendChild(txt)
 
-    todo.insertAdjacentHTML('afterbegin', newTodo)
     // kosongkan fieldnya lagi
     newText.value = ""
+
+    function createList(text, status = false) {
+        // status ?
+        let newTodo = `<li class="the_task"> 
+                    <span class='newLi' onclick="toggle(this)">  ${text} </span> 
+                    <span onclick="removeItem(this)">  [x] </span>
+                    </li>`
+        todoBox.insertAdjacentHTML('afterbegin', newTodo)
+    }
 
     
 }
 
 function toggle(el) {
-    el.classList.toggle('done')
+    let status = el.classList.toggle('done')
+    syncLocalStorage('UPDATE', el.innerText, status)
 }
 
 function removeItem(el) {
     el.parentElement.remove()
+    syncLocalStorage('DELETE', el.previousElementSibling.innerText.trim())
+    
 }
